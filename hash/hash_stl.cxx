4f36935068948274fb1c094adf0cbf1e9c946e0c
warriorbai <wbai86@gmail.com>
2015-02-03 19:12:31 -0800

    Add stl unordered_map functions

--- hash/hash_stl.cxx
+++ hash/hash_stl.cxx
@@ -15,8 +15,21 @@ int main() {
     while(std::getline(fin, s) ) {
 
         cout<<s<<endl;
-
     }
-
+    unordered_map<int, int> map1;
+    int i = 0;
+    //map1.rehash(101);
+    map1.max_load_factor(100);
+    while(i++ != 100) {
+        map1.insert(make_pair(i,3));
+        cout<<map1.bucket_count()<<" "<<map1.max_bucket_count()<<endl;
+    }
+    for(int i = 0; i < map1.bucket_count();i++) {
+        cout<<map1.bucket_size(i)<<" ";
+    }
+    cout<<endl;
+    cout<<"load factor:"<<map1.load_factor()<<endl;
+    cout<<"max load factor:"<<map1.max_load_factor()<<endl;
     return 0;
+
 }
