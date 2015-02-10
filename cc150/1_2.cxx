0ade2d92a7c74a6bb1495b7e17b80ee3363f0b11
warriorbai <wbai86@gmail.com>
2015-02-10 01:21:42 -0800

    merged

--- cc150/1_2.cxx
+++ cc150/1_2.cxx
@@ -172,7 +172,6 @@ void test_1_7() {
 
 
 //1.8 Assume you have a method isSubstring which checks if one word is a substring of another. 
-//Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring (i.e., “waterbottle” is a rotation of “erbottlewat”). 
 bool isRotation(string& s1, string& s2) {
     if(s1.length() != s2.length())    return false;
     string temp = s1+s1;
